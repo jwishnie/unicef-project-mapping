ee4ca861096b49606a930a3c19fcb9252386f09a
Jeff Wishnie <jeff@wishnie.org>
2009-10-22 16:19:13 +0300

    new file:   maplayers/subtests/__init__.py new file:   maplayers/subtests/homepage_view_tests.py renamed:    maplayers/tests.py -> maplayers/subtests/project_view_tests.py new file:   maplayers/subtests/utils_tests.py modified:   maplayers/tests.py             broke 'tests.py' into several files
    new file:   utils.py
                simple utils like is_empty

---
+++ maplayers/subtests/project_view_tests.py
@@ -0,0 +1,36 @@
+# vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
+
+from django.test import TestCase
+from django.test.client import Client
+from maplayers.models import Project
+
+class ProjectsPage(TestCase):
+    def test_should_get_list_of_projects(self):
+        web_client = Client()
+        response = web_client.get('/projects/', {})
+        self.assertEquals(200, response.status_code)
+
+class ProjectPage(TestCase):
+    fixtures=['test_projects_data.json']
+    def test_should_get_project_page(self):
+        webclient = Client()
+        response = webclient.get('/projects/id/1/')
+        self.assertEquals(200, response.status_code)
+    def test_should_return_404_if_project_doesnot_exist(self):
+        webclient = Client()
+        response = webclient.get('/projects/1id/000/')
+        self.assertEquals(404, response.status_code)
+        
+    def test_should_return_list_of_projects_in_bounding_box(self):
+        webclient = Client()
+        context = webclient.get('/projects/bbox/0/0/40/10/').context
+        self.assertEquals(40, context['right'])
+        self.assertEquals(10, context['top'])
+        self.assertEquals(0, context['bottom'])
+        self.assertEquals(0, context['left'])
+        self.assertEquals(Project.objects.get(id=1), context['projects'][0])
+        
+    def test_should_return_list_of_projects_in_selected_sectors(self):
+        webclient = Client()
+        context = webclient.post('/projects/bbox/-180/-90/180/90/', {'2':'environment', '3':'economy'}).context
+        self.assertEquals(set(Project.objects.filter(id__in=[2, 3, 4])), set(context['projects']))
