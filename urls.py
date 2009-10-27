712f4ddca8b9b4b64d704883fc07155819a2f13b
Jeff Wishnie <jeff@wishnie.org>
2009-10-27 14:03:03 +0300

    modified:   urls.py             Only serve static files when in Debug mode

--- urls.py
+++ urls.py
@@ -1,7 +1,8 @@
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
 
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import patterns
 from django.views.generic.simple import direct_to_template
+from django.conf import settings
 from maplayers import views
 from maplayers import admin_view
 
@@ -9,16 +10,22 @@ from maplayers import admin_view
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    (r'^admin/(.*)', admin.site.root),
-    (r'^static/(?P<path>.*)$', 'django.views.static.serve',
-        {'document_root': 'static'}),
-	(r'^projects/$', views.projects),
-    (r'^projects/bbox/(?P<left>.+)/(?P<bottom>.+)/(?P<right>.+)/(?P<top>.+)/$', views.projects_in_map),
-    (r'^projects/id/(?P<project_id>\d+)/$', views.project),
-    (r'^gallery/(?P<gallery_type>\w+)?', views.gallery),
-	(r'^$', views.homepage),
-	(r'^add_project/', admin_view.add_project),
-	(r'^project_created_successfully/', direct_to_template, {'template': 'project_created_successfully.html'}),
-)
+pats = [
+        (r'^admin/(.*)', admin.site.root),
+        (r'^projects/$', views.projects),
+        (r'^projects/bbox/(?P<left>.+)/(?P<bottom>.+)/(?P<right>.+)/(?P<top>.+)/$', views.projects_in_map),
+        (r'^projects/id/(?P<project_id>\d+)/$', views.project),
+        (r'^gallery/(?P<gallery_type>\w+)?', views.gallery),
+        (r'^$', views.homepage),
+        (r'^add_project/', admin_view.add_project),
+        (r'^project_created_successfully/', direct_to_template, {'template': 'project_created_successfully.html'})
+    ]
+
+# If in debug mode, server statics locally, otherwise the host HTTP server should do this
+if settings.DEBUG:
+    static_serve = (r'^static/(?P<path>.*)$', 'django.views.static.serve',
+                    {'document_root': 'static'})
+    pats.insert(0,static_serve)
+    
+urlpatterns = patterns('', *pats)
 
