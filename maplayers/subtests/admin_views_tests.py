0d3c952e4ea645702f6f6b009e638ed8451fcefd
chirdeepshetty <chirdeep.shetty@gmail.com>
2009-11-12 10:31:37 +0530

    Change  password tests

--- maplayers/subtests/admin_views_tests.py
+++ maplayers/subtests/admin_views_tests.py
@@ -159,16 +159,33 @@ class AdminViewsFunctionalTest(TestCase):
         groups = Group.objects.filter(name__in=['editors_publishers', 'admins'])
         self.assertEquals(set(groups), set(user.groups.all()))
         
-        def test_confirm_password_should_match_password(self):
-            web_client = Client()
-            web_client.login(username='map_super', password='map_super')
-
-            response = web_client.post("/user_registration/",
-                            {"username" : "user2",
-                             "email" : "e@e.com",
-                             "password" : "password1",
-                             "confirm_password" : "password",
-                             "groups" : "editors_publishers, admins"}, follow=True)
-            self.assertContains(response, "Passwords do not match")
-            self.assertFalse(User.objects.filter(username="user2"))
+    def test_confirm_password_should_match_password(self):
+        web_client = Client()
+        web_client.login(username='map_super', password='map_super')
+   
+        response = web_client.post("/user_registration/",
+                        {"username" : "user2",
+                         "email" : "e@e.com",
+                         "password" : "password1",
+                         "confirm_password" : "password",
+                         "groups" : "editors_publishers, admins"}, follow=True)
+        self.assertContains(response, "Passwords do not match")
+        self.assertFalse(User.objects.filter(username="user2"))
+        
+    
+    def test_change_password_for_user(self):
+        web_client = Client()
+        self.assertTrue(web_client.login(username='map_super', password='map_super'))
+        web_client.post("/change_password/", 
+                        {"old_password" : "map_super",
+                         "new_password" : "new_password",
+                         "confirm_password" : "new_password"})
+        web_client.logout()
+        self.assertTrue(web_client.login(username='map_super', password='new_password'))
+        web_client.post("/change_password/", 
+                       {"old_password" : "new_password",
+                        "new_password" : "map_super",
+                        "confirm_password" : "map_super"})
+           
+           
     
