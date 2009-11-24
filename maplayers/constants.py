6596462ae8e955ef70e1a272588e1a611a197b0a
saivenkat <s.sai.venkat@gmail.com>
2009-11-24 17:07:34 +0800

    Advocating team ownership

--- maplayers/constants.py
+++ maplayers/constants.py
@@ -1,13 +1,5 @@
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
-import re
-
-'''
-Constants for use anywhere in MapLayers
-
-Created on Oct 29, 2009
-
-@author: jeff wishnie
-'''
+import re as regex
 
 class GROUPS(object):
     ADMINS = u'admins'
@@ -26,4 +18,4 @@ class COMMENT_STATUS(object):
     PUBLISHED = u'Published'
     UNMODERATED = u'UnModerated'
     
-EMAIL_REGEX = re.compile("^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$")
+EMAIL_REGEX = regex.compile("^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$")
