0ca25fc7384672c18138763cfe35854a3226dd25
chirdeepshetty <chirdeep.shetty@gmail.com>
2009-11-26 09:29:40 +0530

    added video URL to add project page

--- static/js/add_project.js
+++ static/js/add_project.js
@@ -11,6 +11,15 @@ jQuery(document).ready(function(){
 		autoFill: true
 	});
 
+    function add_video(){
+        video_id += 1;
+        var div_element = '<div id="video_url_' + video_id + '">';
+        div_element +=  '<label>Video URL : </label>';
+        div_element +=  '<input type="text" name="video_url_' + video_id + '"></input>';
+        div_element +=  '<input type="radio" name="default_video" value="video_'+ video_id + '"></input>';
+        div_element +=  '</div>';
+        jQuery("#video_urls").append(div_element);
+    }
 	
 	function add_link(){
 		link_id +=1;
@@ -24,7 +33,9 @@ jQuery(document).ready(function(){
 	
 	jQuery("#project-links").html(project_links);
 	
-	jQuery("#add_link").click(add_link);
+    jQuery("#add_link").click(add_link);
+    
+    jQuery("#add_video").click(add_video);
 	
 	jQuery('.file-remove-edit').click(function(){
 		var filename = jQuery(this).prev().prev().html();
