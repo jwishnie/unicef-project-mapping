52d3b4dd0ddae35af6167b779eb3b57dce4a3ac1
chirdeepshetty <chirdeep.shetty@gmail.com>
2009-12-14 13:42:18 +0530

    changed base layer on addproject

--- static/js/add_project.js
+++ static/js/add_project.js
@@ -45,16 +45,19 @@ jQuery(document).ready(function(){
     
     map = new OpenLayers.Map('map');
     
+    var layer = new OpenLayers.Layer.WMS( "OpenLayers WMS", "http://labs.metacarta.com/wms/vmap0", {layers: 'basic'},{'displayInLayerSwitcher':false} );
+    
+    
     markers = new OpenLayers.Layer.Markers( "Markers" );
 	map.addLayer(markers);
 	var size = new OpenLayers.Size(10,17);
 	var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
 	var icon = new OpenLayers.Icon('/static/img/bright_red_marker.png',size,offset);
     
-    var ol_wms = new OpenLayers.Layer.VirtualEarth("Hybrid", {
-        type: VEMapStyle.Hybrid
-    });
-    map.addLayer(ol_wms);
+    // var ol_wms = new OpenLayers.Layer.VirtualEarth("Hybrid", {
+    //     type: VEMapStyle.Hybrid
+    // });
+    map.addLayer(layer);
     if (!map.getCenter()) map.zoomToMaxExtent();
     var click = new OpenLayers.Control.Click();
     map.addControl(click);
